cf6b01fd705caa0a36058726c3270155a4536d79
Zoltan Puchovsky <zoltan.puchovsky@gmail.com>
2019-07-14 21:13:55 +0200

    Created Picker View

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dayTextField: UITextField!
+    
+    let days = ["Squat",
+                "Overhead Press",
+                "Deadlift",
+                "Bench Press"]
+    
+    var selectedDay: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDayPicker()
         // Do any additional setup after loading the view.
     }
+    
+    func createDayPicker() {
+        let dayPicker = UIPickerView()
+        dayPicker.delegate = self
+        
+        dayTextField.inputView = dayPicker
+        
+    }
 
+}
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return days.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return days[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        selectedDay = days[row]
+        dayTextField.text = selectedDay
+    }
+    
 }
 
